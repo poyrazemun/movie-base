785382163d4b5d48551f8fa24d7dd3d05c8fdf47
bibakunin <ofatih-poyraz@hotmail.com>
2025-02-12 15:53:17 +0100

    add/delete functions are added

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,24 +3,140 @@ const startAddMovieButton = document.querySelector("header button");
 const backdrop = document.getElementById("backdrop");
 
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive");
+const addMovieButton = addMovieModal.querySelector(".btn--success");
 
-const toggleMovieModal = () => {
-  addMovieModal.classList.toggle("visible");
+const userInputs = addMovieModal.querySelectorAll("input");
+
+const entryTextSection = document.getElementById("entry-text");
+const deleteMovieModal = document.getElementById("delete-modal");
+
+const movies = [];
+
+const updateUI = () => {
+  if (movies.length > 0) {
+    entryTextSection.style.display = "none";
+  }
+};
+
+const deleteMovie = (movieId) => {
+  movies.splice(
+    movies.findIndex((movie) => movie.id === movieId),
+    1
+  );
+
+  document.querySelector(`[data-id="${movieId}"]`).remove();
+
+  if (movies.length === 0) {
+    entryTextSection.style.display = "block";
+  }
+};
+
+const cancelMovieDeletion = () => {
   toggleBackdrop();
+  deleteMovieModal.classList.remove("visible");
+};
+
+const deleteMovieHandler = (movieId) => {
+  deleteMovieModal.classList.add("visible");
+  toggleBackdrop();
+  const cancelDeletionButton = deleteMovieModal.querySelector("btn--passive");
+  const confirmDeletionButton = deleteMovieModal.querySelector("btn--danger");
+  cancelDeletionButton.addEventListener("click", cancelMovieDeletion);
+  confirmDeletionButton.addEventListener(
+    "click",
+    deleteMovie.bind(null, movieId)
+  );
+};
+
+const renderNewMovieElement = (id, title, image, rating) => {
+  const newMovieElement = document.createElement("li");
+  newMovieElement.className = "movie-element";
+  newMovieElement.setAttribute("data-id", id);
+  newMovieElement.innerHTML = `
+  <div class = "movie-element__image">
+  <img src="${image}" alt="${title}"> 
+  </div>
+  <div class="movie-element__info">
+  <h2>${title}</h2>
+  <p>${rating}/5 stars</p>
+  <button class="delete-btn" data-id="movieId">Delete</button>
+  </div>
+  `;
+
+  newMovieElement
+    .querySelector(".delete-btn")
+    .addEventListener("click", deleteMovieHandler.bind(null, id));
+
+  document.getElementById("movie-list").append(newMovieElement);
+};
+
+const closeMovieModal = () => {
+  addMovieModal.classList.remove("visible");
+};
+
+const showMovieModal = () => {
+  addMovieModal.classList.add("visible");
+  toggleBackdrop();
+};
+
+const clearMovieInputs = () => {
+  for (const userInput of userInputs) {
+    userInput.value = "";
+  }
 };
 
 const toggleBackdrop = () => {
   backdrop.classList.toggle("visible");
 };
 
+const addMovieToTheDatabase = () => {
+  const titleValue = userInputs[0].value;
+  const imageUrlValue = userInputs[1].value;
+  const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === "" ||
+    imageUrlValue.trim() === "" ||
+    ratingValue.trim() === "" ||
+    parseInt(ratingValue) < 1 ||
+    parseInt(ratingValue) > 5
+  ) {
+    alert("Please enter valid values!");
+    return;
+  }
+
+  const newMovie = {
+    id: Math.random().toString(),
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue,
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
+  closeMovieModal();
+  toggleBackdrop();
+  clearMovieInputs();
+  renderNewMovieElement(
+    newMovie.id,
+    newMovie.title,
+    newMovie.image,
+    newMovie.rating
+  );
+  updateUI();
+};
+
 const backdropClickHandler = () => {
-  toggleMovieModal();
+  closeMovieModal();
+  cancelMovieDeletion();
 };
 
 const cancelAddMovie = () => {
-  toggleMovieModal();
+  closeMovieModal();
+  clearMovieInputs();
 };
 
-startAddMovieButton.addEventListener("click", toggleMovieModal);
+startAddMovieButton.addEventListener("click", showMovieModal);
 backdrop.addEventListener("click", backdropClickHandler);
 cancelAddMovieButton.addEventListener("click", cancelAddMovie);
+addMovieButton.addEventListener("click", addMovieToTheDatabase);
